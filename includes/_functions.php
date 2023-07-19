9873449f588dca66e70d98c78a0a636e7f6005f6
Tobias Doboszewicz <tobias13@hotmail.fr>
2023-07-19 10:22:31 +0200

    link the database

--- includes/_functions.php
+++ includes/_functions.php
@@ -1,16 +1,18 @@
 <?php
 
 function getList(array $array): string {
-$html = '    <tr>
+    $html = "";
+    foreach($array as $payment)
+$html .= '    <tr>
     <td width="50" class="ps-3">
     </td>
     <td>
-        <time datetime="2023-07-10" class="d-block fst-italic fw-light">'. .'</time>
-        '. .'
+        <time datetime="2023-07-10" class="d-block fst-italic fw-light">'.$payment['date_transaction'].'</time>
+        '.$payment['name'].'
     </td>
     <td class="text-end">
         <span class="rounded-pill text-nowrap bg-warning-subtle px-2">
-            '. .'
+            '.$payment['amount'].'
         </span>
     </td>
     <td class="text-end text-nowrap">
@@ -22,10 +24,7 @@ $html = '    <tr>
         </a>
     </td>
 </tr>';
-
-
-return $html
-
+return $html;
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
+require "includes/_database.php";
+require "includes/_functions.php";
+$query = $dbCo->prepare('SELECT name, amount, date_transaction FROM transaction ORDER BY amount DESC');
+$query->execute();
+$payments = $query->fetchAll();
+
 $title = "Juillet";
 require "includes/_head.php";
 require "includes/_headerandnav.php";
 require "includes/_sold.php";
+echo getList($payments);
+
 
 
 
